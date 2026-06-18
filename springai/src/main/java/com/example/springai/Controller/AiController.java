ae0fa88650a313f032e5ecaf658a20c5dd8f5241
Ashish Admin <ashish.admin@gmail.com>
2026-06-18 16:30:23 +0530

    added new mapping

--- springai/src/main/java/com/example/springai/Controller/AiController.java
+++ springai/src/main/java/com/example/springai/Controller/AiController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -16,5 +17,8 @@ public class AiController {
         return ResponseEntity.ok().body(response);
         
     }
-    
+     @GetMapping("/user/{id}")
+    public String getUserById(@PathVariable int id) {
+        return "User ID is: " + id;
+    }
 }
